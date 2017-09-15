6542d99a191fbc76792f0e8e1d6da1ce709b1cd0
bhanu97 <p.bannu97@gmail.com>
2017-09-15 09:30:30 +0000

    Update dminer.py

--- dminer.py
+++ dminer.py
@@ -1,12 +1,13 @@
 import sys
 
 list1 = ["for","if","while","else if","else","int","float","void","char"]
+list2 = ["int","float","void","char"]
 
-myfile =open("d.c")
+myfile =open("example.c") # Give the input c file here
 
 
 def myfunc4(num2,key2,count2):
-    myfile4 = open('d.c', 'r').readlines()[num2:]
+    myfile4 = open('example.c', 'r').readlines()[num2:]
     for i4 in myfile4:
         sr = False
         for i5 in i4:
@@ -24,54 +25,51 @@ def myfunc4(num2,key2,count2):
 def myfunc3(num1,key1):
     nb = True
     hp =True
-    bt= True
+    bt= 1 
     count1 = 0 
-    myfile3 = open('d.c', 'r').readlines()[num1:]
-    for i3 in myfile3:
+    myfile3 = open('example.c', 'r').readlines()[num1:]
+    if key1 in list2:
+       bt =3
+       for i3 in myfile3:
         for i4 in i3:
             if i4 =='(':
                nb =False
-            elif i4 ==')':
-                 nb = True
-            elif i4 =='{':
-                 hp =False
-            if not nb and i4 ==';':
-               count1+=1  
-            if nb and hp and i4 ==';':
-               bt =False
+            if i4 ==';':
+               hp = False
+            if not nb and hp and i4 =='{':
+               bt =1 
                break
-        if not bt:
-           break
-    if bt:
+               
+    else:        
+        for i3 in myfile3:
+            for i4 in i3:
+                if i4 =='(':
+                   nb =False
+                elif i4 ==')':
+                     nb = True
+                elif i4 =='{':
+                     hp =False
+                if not nb and i4 ==';':
+                   count1+=1  
+                if nb and hp and i4 ==';':
+                   bt = 0
+                   break
+            if bt==0:
+               break
+               
+    if bt==1:
+       print "\n *****************************************NEW BLOCK******************************************************************"
        myfunc2(num1,key1)
        
-    else:
-        #print count1
+    elif bt==0:
+        print "\n *****************************************NEW BLOCK******************************************************************"
         myfunc4(num1,key1,count1+1)
            
-'''def myfunc5():
-    myfile5 =open("d.c")
-    for i in list2:
-        nb1 = True
-        hp1 =True
-        bt1 = True
-        for j in myfile5:
-            if i in j:
-               for k in j:
-                   if k=='(':
-                      nb1 =False
-                   elif k==')':
-                        nb1 =True
-                   if k==';' and nb1:
-                        hp1 =
-               
- '''      
-
-
+ 
 def myfunc2(num,key):
-    brac = 1    # num of bracs
+    brac = 1    
     c = False   
-    myfile2 = open('d.c', 'r').readlines()[num:]
+    myfile2 = open('example.c', 'r').readlines()[num:]
     for i1 in myfile2:
         for i2 in i1:
             if i2 =='{':
@@ -85,21 +83,20 @@ def myfunc2(num,key):
         
 
 def myfunc(myfile):
-    count = 0   #line num
+    count = 0   
     for i in myfile:
         count+=1
         for j in list1:
             if j=="else":
                if j in i and "if"  not in i:
-                  print "\n *****************************************NEW BLOCK******************************************************************"
                   myfunc3(count-1,j)
             elif j=="if":
                  if j in i and "else"  not in i:
-                    print "\n *****************************************NEW BLOCK******************************************************************"
                     myfunc3(count-1,j)
             else:
-                if j in i:
-                   print "\n *****************************************NEW BLOCK******************************************************************"
+                if j in i:  
                    myfunc3(count-1,j)
+                   
 myfunc(myfile)
+
 
